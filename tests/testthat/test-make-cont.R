be28e6f6efdf68de485ce95318006a6e8807003e
myan0065 <80817413+myan0065@users.noreply.github.com>
2022-07-30 12:14:20 +1000

    add amalgamation test for make.cont

--- tests/testthat/test-make-cont.R
+++ tests/testthat/test-make-cont.R
@@ -12,15 +12,21 @@ test_that("check categorical variables are continulized", {
 
 
 
-# test_that("check categorical variables are continulized with amalgamations", {
-#   Data_full <- ISLR::Wage
-#   Data <- Data_full %>%
-#     dplyr::select(age, maritl, race, education, jobclass, wage)
-#   Data_x <- make.cont(Data,
-#                       catvar = c("maritl","race","education","jobclass"),
-#                       amalgams = list(1:2,3:4))
-#
-#   expect_equal() #???? reorder first
-#   make.cat....
-# })
+test_that("check categorical variables are continulized with amalgamations", {
+  Data_full <- ISLR::Wage
+  Data <- Data_full %>%
+    dplyr::select(age, maritl, race, education, jobclass, wage)
+  Data_amal <- make.cont(Data,
+                      catvar = c("maritl","race","education","jobclass"),
+                      amalgams = list(1:2,3:4))
+
+  expect_equal(
+    stringr::str_sort(colnames(make.cat(Data_amal))),
+    stringr::str_sort(colnames(Data))
+      )
+
+})
+
+#> Test passed!
+
 

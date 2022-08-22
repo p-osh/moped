5bb9913d2182074c8688d955493c8b33d581a2b8
p-oshaughnessy <poshaugh@uow.edu.au>
2022-08-22 14:26:01 +1000

    Update m.resample.R

--- R/m.resample.R
+++ R/m.resample.R
@@ -45,10 +45,10 @@
 #'
 #' # Generating resampled (synthetic) observations
 #' # Sample 100 obs from moped joint density estimate without updating "age"
-#' resampled <- m.resample(Fit, K = 3, Sample = Data_x, n = 100, fixed.var = "age")
+#' m.resample(Fit, K = 3, Sample = Data_x, n = 100, fixed.var = "age")
 #'
 #' # Simulate a fully resampled data set of same size as Data_x.
-#' resampled <- m.resample(Fit, K = 3, replicates = 2) # 2 Gibbs passes used.
+#' m.resample(Fit, K = 3, replicates = 2) # 2 Gibbs passes used.
 #'
 #' # Simulate a fully resampled data set of same size as Data.
 #' m.resample(Fit, K=3, replicates = 2) # 2 Gibbs passes used.
